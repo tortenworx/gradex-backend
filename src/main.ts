cd50923485db7edcef1fe86307a0c4d09e64b448
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2024-11-08 02:01:32 +0800

    fix!(cors) fucking cors part 2 [forgot to actually apply changes]

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
+  app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
